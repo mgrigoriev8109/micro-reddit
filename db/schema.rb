8caf2b2f086f9093412f2b9b0330c8c499bc0859
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-06 14:43:03 -0500

    Add new migration to Post
    Which adds a user_id column and index to Posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_06_184110) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_06_185838) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "context"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
