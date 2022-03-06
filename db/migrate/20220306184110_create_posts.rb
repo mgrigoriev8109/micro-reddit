bdfe3c758cb09095e61793871f5c427bc3aaffec
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-06 13:53:44 -0500

    Fix error in CreatePosts migration
    Error in arrangment of t.string and :title was fixed, and schema was updated by performing migration

--- db/migrate/20220306184110_create_posts.rb
+++ db/migrate/20220306184110_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.title :string
-      t.content :text
+      t.string :title
+      t.text :context
 
       t.timestamps
     end
