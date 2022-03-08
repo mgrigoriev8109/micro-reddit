ffa70ceadf224ad61430721ca746a948200ea79f
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-07 22:07:13 -0500

    Finish creating association between Comments and User / Post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :posts
-    
+    has_many :comments
+
     validates :name, :password, :email, presence: true
     validates :name, :email, uniqueness: true
 end
